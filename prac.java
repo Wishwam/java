25c15c03173b18eb76eee94a163225e1297b009b
Wishwam <wishwam0511@gmail.com>
2025-08-30 10:48:10 +0530

    basic

---
+++ prac.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class prac {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    // ----sum of numbers----
+        // System.out.println("Enter first num: ");
+        // int a = sc.nextInt();
+        // System.out.println("Enyter second num: ");
+        // int b = sc.nextInt();
+        // System.out.println("Enter third num: ");
+        // int c = sc.nextInt();
+        // int sum = a+b+c;
+        // System.out.println("Sum : " +sum);
+
+    // ----percentage----
+        // System.out.println("Enter marks of 1st sub: ");
+        // int p = sc.nextInt();
+        // System.out.println("Enter marks of 2nd sub: ");
+        // int q = sc.nextInt();
+        // System.out.println("Enter marks of 3rd sub: ");
+        // int r = sc.nextInt();
+        // System.out.println("Enter max marks of each sub: ");
+        // int m = sc.nextInt();
+
+        // double mo = (p+q+r);
+        // double max = m*3;
+        // double per = (mo / max) *100;
+        
+        // System.out.println("Percentage of student: " + per);
+
+
+    // ---greet---
+        System.out.println("Enter your name: ");
+        String a = sc.nextLine();
+        System.out.println("Hello " + a + "!");
+        sc.close();
+    }
+}
+// public class demo
+// {
+//     public static void main(String[] args){
+//         int a =15;
+//         int b;
+//         b= ++a + ++a;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
